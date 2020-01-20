23d4075dbc4abc4dae5124c01aa8fe9f20618e9c
Lauren <laurenelizamax@icloud.com>
2020-01-20 14:41:05 -0600

    first commit

---
+++ EFWorkforce/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFWorkforce.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Department Name")]
+        public string Name { get; set; }
+        public int Budget { get; set; }
+
+        [Display(Name = "Size of Department")]
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
